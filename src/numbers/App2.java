f4ba9d2fd4040a70c584d3a3ddbea00e2535631d
Etienne Dehm <etde0001@stud.hs-kl.de>
2020-09-24 21:56:01 +0200

    Neue Klasse für Schritt 2 namens App2.

---
+++ src/numbers/App2.java
@@ -0,0 +1,11 @@
+package numbers;
+
+public class App2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hier finden Sie die Anwendungsklasse zu Schritt 2 der Abgabe.");
+		Rational bruch = new Rational();
+		bruch.eingabe();
+	}
+
+}
